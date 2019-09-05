098fc150f0f073a66de1c6d009e35f4754f207f9
Andy Shih <AndyShih51@gmail.com>
2019-09-05 00:40:39 -0500

    Edit code formatting

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python3
 
 import json
-
-from flask import (Flask, render_template, redirect,
-                   url_for, request, make_response,
-                   flash)
-
+from flask import (Flask, render_template, redirect, url_for,
+                   request, make_response, flash)
 from options import DEFAULTS
 
 app = Flask(__name__)
 app.secret_key = 'esauhou>UO>au.sh35@<Uouo52%@#ouo.42!@#42'
-
 
 def get_saved_data():
     try:
@@ -18,34 +14,30 @@ def get_saved_data():
     except TypeError:
         data = {}
     return data
-                
-
 
 @app.route('/')
 def index():
     return render_template('index.html', saves=get_saved_data())
-
 
 @app.route('/builder')
 def builder():
     return render_template(
         'builder.html',
         saves=get_saved_data(),
-        options=DEFAULTS
-    )
+        options=DEFAULTS)
 
 @app.route('/save', methods=['POST'])
 def save():
     flash("Alright! That looks awesome!")
+    # Make a redirect response
     response = make_response(redirect(url_for('builder')))
+    # load data from cookie
     data = get_saved_data()
+    # Update data with values from POST request
     data.update(dict(request.form.items()))
+    # Save data to cookie for the response
     response.set_cookie('character', json.dumps(data))
     return response
 
-@app.route('/doge')
-def doge():
-    return '1 doge 2 doge 3 doge WOW'
-
-if __name__ == "main":
+if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
